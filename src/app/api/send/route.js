6bb96b63b4fc5a574bed5c70c8e835a87871ae63
mingke-utas <mingked@utas.edu.au>
2023-10-17 23:33:07 +1100

    API key

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend("re_eD16AhAx_MMtsmTtKZ24NnppR37RTEojr");
 const fromEmail = process.env.FROM_EMAIL
 
 export async function POST(req, res) {
